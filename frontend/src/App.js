ef8b1cc046d952e85e6af2a951deac735ca65627
sg476 <sid.gupta476@gmail.com>
2020-12-02 12:17:13 +0530

    FrontEnd | Avatar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import Avatar from '@material-ui/core/Avatar';
 
 function App() {
   return (
     <div className="App">
-      hello
+      <div className="container-fluid">
+        <div className="row my-auto">
+          <div className="col-4 offset-4 shadow">
+            <b className="text-center">Upload your profile pic</b>
+            <Avatar alt="Natacha" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
